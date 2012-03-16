16175b0cd74ab12e13de1c564de1f0fc580e5836
Per Bernhardt <plb@webfactory.de>
2012-03-16 20:05:14 +0100

    Standardedition eingerichtet

--- web/app_dev.php
+++ web/app_dev.php
@@ -1,2 +1,13 @@
 <?php
 
+require_once __DIR__.'/../app/bootstrap.php.cache';
+require_once __DIR__.'/../app/AppKernel.php';
+
+use Symfony\Component\HttpFoundation\Request;
+
+$kernel = new AppKernel('dev', true);
+$kernel->loadClassCache();
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
